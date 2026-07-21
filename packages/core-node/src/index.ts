166957ddd90efe3880ae7651f661c9ee4568a21b
Lamine <lamine.gaye@yoniafrica.com>
2026-07-21 17:39:40 +0000

    fix: prepare packages for release publishing

---
+++ packages/core-node/src/index.ts
@@ -0,0 +1,25 @@
+export { PayAfrica } from "./PayAfrica.js";
+export {
+  PaymentError,
+  PaymentStatus,
+  type PaymentEvent,
+  type PaymentProvider,
+  type PaymentRequest,
+  type PaymentSession,
+  type RefundResult,
+} from "./types.js";
+export {
+  OrangeMoneyProvider,
+  OrangeMoneyProviderError,
+  type OrangeMoneyProviderConfig,
+} from "./providers/orange-money.js";
+export {
+  WaveProvider,
+  WaveProviderError,
+  type WaveProviderConfig,
+} from "./providers/wave.js";
+export {
+  MtnMomoProvider,
+  MtnMomoProviderError,
+  type MtnMomoProviderConfig,
+} from "./providers/mtn-momo.js";
